2fcd53cbf523c2cf05627fb91bb351ebd7ccb6aa
mlgibson8 <michael.l.gibson@du.edu>
2023-08-03 16:11:07 -0600

    setup apollo server

--- server/config/config.js
+++ server/config/config.js
@@ -6,5 +6,13 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mlgibson8:abcdefg@clu
   useCreateIndex: true,
   useFindAndModify: false,
 });
+/* 
+const { connect, connection } = require("mongoose");
+connect("mongodb://127.0.0.1:27017/visionaryVault", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+module.exports = connection; */
 
 module.exports = mongoose.connection;
