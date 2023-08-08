ef87deaf5ae1319232d0dd6d1531274268fe958f
kit <dhinkit@gmail.com>
2023-08-07 18:46:29 -0600

    fixed closing tag issue

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mlgibson8:abcdefg@cluster0.5a988ya.mongodb.net', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-// });
- 
-const { connect, connection } = require("mongoose");
-connect("mongodb://127.0.0.1:27017/visionaryVault", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mlgibson8:abcdefg@cluster0.5a988ya.mongodb.net', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
+
+// const { connect, connection } = require("mongoose");
+// connect("mongodb://127.0.0.1:27017/visionaryVault", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 module.exports = connection; 
 
